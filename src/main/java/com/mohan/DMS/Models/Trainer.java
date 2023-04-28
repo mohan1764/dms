bc4f677e7cbf24a5ef8c4ee818ba72239353a39d
mohan1764 <s554815@nwmissouri.edu>
2023-04-28 15:03:48 -0500

    first commit

--- src/main/java/com/mohan/DMS/Models/Trainer.java
+++ src/main/java/com/mohan/DMS/Models/Trainer.java
@@ -1,7 +1,7 @@
 package com.mohan.DMS.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Trainer {
