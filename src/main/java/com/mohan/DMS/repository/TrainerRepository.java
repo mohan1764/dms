3edf0e2205f531f783db8b4c6bb7b6b36bd6cf7a
mohan1764 <s554815@nwmissouri.edu>
2023-04-28 14:58:00 -0500

    first commit

---
+++ src/main/java/com/mohan/DMS/repository/TrainerRepository.java
@@ -0,0 +1,7 @@
+package com.mohan.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.mohan.DMS.Models.Trainer;
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
